cd477f7b512c75a7641e3ffb699659eb8c912e70
prasanna <>
2019-01-05 10:26:18 +0530

    created a form

--- src/components/App/view/index.js
+++ src/components/App/view/index.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React from "react";
+import Form from "../form/index"
 
 
 
@@ -6,10 +7,12 @@ class View extends React.Component {
     render() {
         return(
             <section>
+              <Form/>
                 <h2>View detail</h2>
                 <div>Name: {this.props.name}</div>
                 <div>Alias: {this.props.alias}</div>
                 <div>Team: {this.props.team}</div>
+                
             </section>
         )
     }
